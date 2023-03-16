042b8dee8ccd1e67903ce94a968f033fb7561486
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-03-16 13:37:33 -0400

    Update oauth2.py
    Make the OAuth2 flow more robust, handle invalid/expired refresh_token

--- oauth2.py
+++ oauth2.py
@@ -17,27 +17,34 @@ class OAuth2Handler:
             with open(self.token_file, 'r') as f:
                 token = json.load(f)
 
-            if token['expires_at'] > time.time():
+            if token['expires_at'] > time.time() and 'access_token' in token:
                 return token['access_token']
 
+            elif 'refresh_token' in token:
+                new_token = self.refresh_access_token(token['refresh_token'])
+                if 'access_token' in new_token:
+                    with open(self.token_file, 'w') as f:
+                        json.dump(new_token, f)
+                    return new_token['access_token']
+                else:
+                    print("Refresh token is invalid or expired. Please reauthorize the application.")
+                    os.remove(self.token_file)
             else:
-                token = self.refresh_access_token(token['refresh_token'])
-                with open(self.token_file, 'w') as f:
-                    json.dump(token, f)
-                return token['access_token']
+                print("Refresh token not found. Please reauthorize the application.")
+                os.remove(self.token_file)
 
-        else:
-            code_request = f"{self.authorization_url}?response_type=code&client_id={self.client_id}&scope=contact_data&access_type=offline&redirect_uri=https://localhost&state=randomstate"
-            print(f"Please go to this URL and authorize the application: {code_request}")
-            callback_url = input("Enter the full callback URL: ")
+        # If the token file does not exist or has been removed due to errors, request a new access token
+        code_request = f"{self.authorization_url}?response_type=code&client_id={self.client_id}&scope=contact_data&access_type=offline&redirect_uri=https://localhost&state=randomstate"
+        print(f"Please go to this URL and authorize the application: {code_request}")
+        callback_url = input("Enter the full callback URL: ")
 
-            code = self.parse_code(callback_url)
-            token = self.request_access_token(code)
+        code = self.parse_code(callback_url)
+        token = self.request_access_token(code)
 
-            with open(self.token_file, 'w') as f:
-                json.dump(token, f)
+        with open(self.token_file, 'w') as f:
+            json.dump(token, f)
 
-            return token['access_token']
+        return token['access_token']
 
     def parse_code(self, callback_url):
         query = callback_url.split('?')[1]
@@ -72,5 +79,11 @@ class OAuth2Handler:
         }
         response = requests.post(self.token_url, auth=(self.client_id, self.client_secret), headers=headers, data=token_request_data)
         token_data = response.json()
-        token_data['expires_at'] = time.time() + token_data['expires_in']
+        
+        if 'expires_in' in token_data:
+            token_data['expires_at'] = time.time() + token_data['expires_in']
+        else:
+            # handle missing 'expires_in' here, e.g., set a default value or log a warning
+            token_data['expires_at'] = time.time() + 86400
+
         return token_data
