214d338ee1bf594dced2d1ddec6d93606326f69b
Justin Hayes <justinh@rahb.ca>
2023-08-17 16:25:42 -0400

    Fix auth URL string construction

--- config.example.py
+++ config.example.py
@@ -11,5 +11,5 @@ LIST_ID = "YOUR_LIST_ID_HERE"
 CLIENT_ID = 'YOUR_CLIENT_ID'
 CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
 REDIRECT_URI = 'http://127.0.0.1:5000/callback'  # Adjust based on where your application is hosted
-AUTHORIZATION_URL = f'https://authz.constantcontact.com/oauth2/default/v1/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}'
+AUTHORIZATION_URL = f'https://authz.constantcontact.com/oauth2/default/v1/authorize'
 TOKEN_URL = 'https://authz.constantcontact.com/oauth2/default/v1/token'
