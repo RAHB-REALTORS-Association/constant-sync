09f816aca7e704c7272f0e8024e8b02c3a0c8c58
Justin Hayes <justinh@rahb.ca>
2023-08-18 08:59:13 -0400

    Manual sync

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from flask import Flask, render_template
 from threading import Thread
+from synchronizer import synchronize_contacts
 from scheduler import start_scheduled_synchronization
 from oauth2 import oauth2_blueprint
 import logging
@@ -10,17 +12,27 @@ app = Flask(__name__)
 app.register_blueprint(oauth2_blueprint)
 app.secret_key = 'some_secure_random_string'  # Please use a secure secret key in production
 
+# Global variable to hold last sync status
+last_sync_status = None
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', last_sync=last_sync_status)
 
-def run_sync_service():
-    logging.info("Starting synchronization service...")
-    start_scheduled_synchronization(2)  # Every 2 hours (this can be configured)
+@app.route('/manual_sync', methods=["POST"])
+def manual_sync():
+    global last_sync_status
+    try:
+        synchronize_contacts()
+        last_sync_status = f"Last successful sync at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        return render_template('success.html', message="Manual synchronization initiated successfully.")
+    except Exception as e:
+        last_sync_status = f"Error during sync at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {str(e)}"
+        return render_template('error.html', error_message=f"Error during manual synchronization: {e}")
 
 if __name__ == '__main__':
     # Start synchronization service as a background thread
-    sync_thread = Thread(target=run_sync_service)
+    sync_thread = Thread(target=start_scheduled_synchronization)
     sync_thread.start()
 
     logging.info("Starting Flask application...")
--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 import atexit
+from config import SYNC_FREQUENCY_HOURS
 from synchronizer import synchronize_contacts
 
 scheduler = BackgroundScheduler()
 scheduler.start()
 atexit.register(lambda: scheduler.shutdown())
 
-def start_scheduled_synchronization(interval_hours=2):
+def start_scheduled_synchronization():
     scheduler.add_job(
         func=synchronize_contacts,
-        trigger=IntervalTrigger(hours=interval_hours),
+        trigger=IntervalTrigger(hours=SYNC_FREQUENCY_HOURS),
         id='scheduled_synchronization_job',
         name='Synchronize JSON contacts with Constant Contact',
         replace_existing=True
